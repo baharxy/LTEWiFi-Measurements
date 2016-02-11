10c0909b0ec2d9d7e4dc5bfbd527f1ba4c1f496d
Bahar <bahar@Bahars-MacBook-Air.local>
2016-02-11 17:13:39 +0000

    uplink and downlink TRX

--- Initiate_Downlink.py
+++ Initiate_Downlink.py
@@ -32,21 +32,19 @@ import psutil
 #CLIENT - SENDER
 UDP_DEST_IP='134.226.40.138' #IP ADDRESS TO SEND DATAGRAMS TO (v4 or v6)
 UDP_DEST_PORT=60000 #IP PORT TO SEND DATAGRAMS TO
-PACKET_SIZE = 1500 #DATAGRAM SIZE IN BYTES
-NR_OF_PACKETS=2000 #TOTAL NR. OF PACKETS TO SEND
+PACKET_SIZE = 500 #DATAGRAM SIZE IN BYTES
+NR_OF_PACKETS=1 #TOTAL NR. OF PACKETS TO SEND
 PACKETS_PER_SEC=100 #PACKETS PER SECON
-rate= float(float(PACKET_SIZE) * float(PACKETS_PER_SEC) *  8 / 1000000 )#data rate mbps
-print ('Transmission rate is:  %3f'  %(rate) + ' Mbit/s')
+
 #files and directory assignment
 directory=( '/Users/bahar/Documents/tcpdumpData/python/csv/'+ datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S_%f')[ :-3])
 if not os.path.exists(directory):
     os.makedirs(directory)
 os.chdir(directory)
-file_name='udp_rtt_' + '%s' %(PACKET_SIZE) +'bytes_' +'%s' %(NR_OF_PACKETS) +'packets'+ '%s' %(PACKETS_PER_SEC)+'sec'+ datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S_%f')[ :-3] +'.csv'
-tcpdump_output='tcpdump_' + '%s' %(PACKET_SIZE) +'bytes_' +'%s' %(NR_OF_PACKETS) +'packets'+ '%s' %(PACKETS_PER_SEC)+'sec' + datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S_%f')[ :-3] + '.csv'
+file_name='udp_oneway_downlink_' + '%s' %(PACKET_SIZE) +'bytes_' +'%s' %(NR_OF_PACKETS) +'packets'+ '%s' %(PACKETS_PER_SEC)+'sec'+ datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S_%f')[ :-3] +'.csv'
+#tcpdump_output='tcpdump_' + '%s' %(PACKET_SIZE) +'bytes_' +'%s' %(NR_OF_PACKETS) +'packets'+ '%s' %(PACKETS_PER_SEC)+'sec' + datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S_%f')[ :-3] + '.csv'
 channels_file='channels.csv'
 
-open(channels_file, "a").write('Transmission rate: '+ str(rate)+'  Mbit/s' +'\n')
 
 #get ip addresses of the avialable interfaces
 if 'en0' not in netifaces.interfaces():
@@ -161,7 +159,7 @@ def sniff_wifi():
 
 
 #CLIENT-RECEIVER PART
-def udp_client_receive(UDP_RECEIVE_IP, UDP_RECEIVE_PORT,offset,input_sock, interface_type):
+def udp_client_receive(UDP_RECEIVE_IP, UDP_RECEIVE_PORT,offset,input_sock):
  
  global wifi_packet_count_rcvd
  global lte_packet_count_rcvd
@@ -185,15 +183,10 @@ def udp_client_receive(UDP_RECEIVE_IP, UDP_RECEIVE_PORT,offset,input_sock, inter
   #rt_delay = (time.time() - float(timecount))
   packet_number = str(splitdata[1].strip("' '"))
   packet_number = packet_number.lstrip('0')
+  client_interface=str(splitdata[2].strip("' '"))
   #WRITE TO FILE AND DO PACKET COUNT
-  outfile = open(file_name, "a").write(str(time.ctime()+','+'received , '+ packet_number+' , '+str(rt_delay)+ ','+  interface_type+'\n'))
-  #print (time.ctime()+','+'received , '+ packet_number+' , '+ str('%.5f' % timecountnooffset) +'sent time, '+  str('%.5f' % float (actual_time-offset)) + 'received time, ' +str(rt_delay)+'delay')
-  if interface_type=='wifi':
-   wifi_packet_count_rcvd=wifi_packet_count_rcvd+1
-   wifi_cumulative_delay=wifi_cumulative_delay+rt_delay
-  elif interface_type=='lte':
-   lte_packet_count_rcvd=lte_packet_count_rcvd+1
-   lte_cumulative_delay=lte_cumulative_delay+rt_delay
+  outfile = open(file_name, "a").write(str(time.ctime()+','+'received , '+ packet_number+' , '+str(rt_delay)+ ','+  client_interface+'\n'))
+  print (str(time.ctime())+','+'received , '+ packet_number+' , '+ str('%.5f' % timecountnooffset) +'sent time, '+  str('%.5f' % float (actual_time-offset)) + 'received time, ' +str(rt_delay)+'delay')
 
 
   
@@ -204,7 +197,7 @@ def udp_client_send(UDP_DEST_IP, UDP_DEST_PORT, PACKET_SIZE, NR_OF_PACKETS, PACK
  inter_departure_time = 1./PACKETS_PER_SEC
  wifi_packet_count_snd=0
  lte_packet_count_snd=0
- print 'UDP Client Started' , '\n'
+ print 'UDP Initiate send started' , '\n'
  print 'UDP target IP:', UDP_DEST_IP, '\n'
  print 'UDP target port:', UDP_DEST_PORT, '\n'
  print 'UDP Packets to Send:', NR_OF_PACKETS, '\n'
@@ -232,136 +225,40 @@ def udp_client_send(UDP_DEST_IP, UDP_DEST_PORT, PACKET_SIZE, NR_OF_PACKETS, PACK
      padding = padding+str(1)
    for i in range (1,NR_OF_PACKETS+1):
      time.sleep(inter_departure_time)
-     #snd_sock = socket.socket( socket.AF_INET,socket.SOCK_DGRAM )
-     #Try to bind to send socket??
-     #try:
-      #snd_sock.bind(('0.0.0.0',65000))
-      #print ("Clie", ADDR )
-     #except Exception:
-      #print '***ERROR: Client Port Binding Failed'
-     #timestamp+ntp offset
      input_sock.sendto(str(("%.5f" % float(time.time()+ offset),str('%08d' % i), interface_type, padding)), (UDP_DEST_IP, UDP_DEST_PORT) )
-     #print (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]+','+'sent, '+ str('%02d' % i)+ interface_type+'\n')
-     #print (datetime.datetime.now()  +  ',' + 'sent, ' + str('%08d' %i) )
-     #ntp offset is not considered
-     #input_sock.sendto(str(("%.5f" % float(time.time()),str('%08d' % i), padding)), (UDP_DEST_IP, UDP_DEST_PORT) )
-     #was there to log time shift- but may be this adds additional delay so we only do it once
-     #logging.info(txt)
      if interface_type=='wifi':
       wifi_packet_count_snd = wifi_packet_count_snd+1
      elif interface_type=='lte':
       lte_packet_count_snd = lte_packet_count_snd+1
  
- #WAIT 5SEC FOR ALL PACKETS TO ARRIVE
- time.sleep(5)
- if interface_type=='wifi':
-  wifi_PLR = 100 - ((wifi_packet_count_rcvd*100.)/wifi_packet_count_snd)
-  print '\n', wifi_packet_count_snd, 'wifi packets sent','\n'
-  print wifi_packet_count_rcvd, 'wifi packets received','\n'
-  print 'wifi packet loss ratio = ', round(wifi_PLR, 3), '%','\n'
-  if wifi_packet_count_rcvd == 0:
-   pass
-  else:
-   print 'average wifi rtt = ', wifi_cumulative_delay/wifi_packet_count_rcvd,'\n'
- elif interface_type=='lte':
-  lte_PLR = 100 - ((lte_packet_count_rcvd*100.)/lte_packet_count_snd)
-  print '\n', lte_packet_count_snd, 'lte packets sent','\n'
-  print lte_packet_count_rcvd, 'lte packets received','\n'
-  print 'lte packet loss ratio = ', round(lte_PLR, 3), '%','\n'
-  if lte_packet_count_rcvd == 0:
-   pass
-  else:
-   print 'average lte rtt = ', lte_cumulative_delay/lte_packet_count_rcvd,'\n'
 
-
-#function to call tcpdump
-def Readtraffic():
- print 'initialising tcpdump- lte and wifi packet counts'
- global tcpdump_lte_count
- global tcpdump_wifi_count
- tcpdump_lte_count=0
- tcpdump_wifi_count=0
- command = 'sudo tcpdump host 134.226.40.138 udp'
- dumpproc = subprocess.Popen(['sudo', 'tcpdump', '-n', 'dst' ,'host', '134.226.40.138'] , bufsize=0, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
- #dumpproc = subprocess.Popen(command,executable="/bin/bash", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
- global tcpdump_pid
- tcpdump_pid=dumpproc.pid
- print('tcpdump pid is:'+str(tcpdump_pid))
- #output = ''
- for line in iter(dumpproc.stdout.readline, ""):
- #while True:
-  #line=dumpproc.stdout.readline()
-  #if subprocess.Popen.poll(dumpproc) is not None:
-  if len(line.split()) > 2 :
-   line_split=line.split()
-   if line_split[1]=='IP':
-    if line_split[2]== LTE_Addr_string:
-     line_split[2]='lte'
-     tcpdump_lte_count=tcpdump_lte_count+1
-     line_split.append(str(tcpdump_lte_count))
-     new_line= " , ".join(line_split)
-     outfile = open(tcpdump_output, "a").write(new_line+'\n')
-     #print (new_line + '\n')
-    elif line_split[2]==wifi_Addr_string:
-     line_split[2]='wifi'
-     tcpdump_wifi_count=tcpdump_wifi_count+1
-     line_split.append(str(tcpdump_wifi_count))
-     new_line= " , ".join(line_split)
-     outfile = open(tcpdump_output, "a").write(new_line+'\n')
-     #print (new_line+ '\n')
-  elif len(line.split())< 2 or  not line:
-   print 'no more data, exiting tcpdump'
-   dumpproc.terminate()
-   break
- print('DONE')
-
-#def kill_processes():
 
 
 
 #START THE THREADS FOR SENDER AND RECEIVER
 if __name__ == "__main__":
     
-    sniff_wifi()
-    sniff_lte()
-    tcpdump_thread=threading.Thread(target=Readtraffic, args=())
-    #tcpdump_thread.daemon=True
-    tcpdump_thread.start()
-    #time.sleep(1)
+    udp_client_send(UDP_DEST_IP, UDP_DEST_PORT, PACKET_SIZE, NR_OF_PACKETS, PACKETS_PER_SEC,offset,wifi_sock,'wifi')
 
+    udp_client_send(UDP_DEST_IP, UDP_DEST_PORT, PACKET_SIZE, NR_OF_PACKETS, PACKETS_PER_SEC,offset,LTE_sock,'lte')
     
     
-    wifi_sender_thread = threading.Thread(target=udp_client_send, args=(UDP_DEST_IP, UDP_DEST_PORT, PACKET_SIZE, NR_OF_PACKETS, PACKETS_PER_SEC,offset,wifi_sock,'wifi'))
-    #wifi_sender_thread.daemon=True
-    wifi_sender_thread.start()
-    
-    LTE_sender_thread = threading.Thread(target=udp_client_send, args=(UDP_DEST_IP, UDP_DEST_PORT, PACKET_SIZE, NR_OF_PACKETS, PACKETS_PER_SEC,offset,LTE_sock,'lte'))
-    #LTE_sender_thread.daemon=True
-    LTE_sender_thread.start()
-    
-    wifi_receiver_thread = threading.Thread(target=udp_client_receive, args=(UDP_RECEIVE_IP_wifi, UDP_RECEIVE_PORT_wifi,offset,wifi_sock,'wifi'))
+    wifi_receiver_thread = threading.Thread(target=udp_client_receive, args=(UDP_RECEIVE_IP_wifi, UDP_RECEIVE_PORT_wifi,offset,wifi_sock))
     wifi_receiver_thread.daemon=True
     wifi_receiver_thread.start()
 
-    LTE_receiver_thread = threading.Thread(target=udp_client_receive, args=(UDP_RECEIVE_IP_LTE, UDP_RECEIVE_PORT_LTE,offset,LTE_sock,'lte'))
+    LTE_receiver_thread = threading.Thread(target=udp_client_receive, args=(UDP_RECEIVE_IP_LTE, UDP_RECEIVE_PORT_LTE,offset,LTE_sock))
     LTE_receiver_thread.daemon=True
     LTE_receiver_thread.start()
     #time.sleep(1)
     #close all threads
-    wifi_sender_thread.join()
-    LTE_sender_thread.join()
 
-    #wifi_receiver_thread.join()
-    #LTE_receiver_thread.join()
-    time.sleep(5)
-    print 'killing tcpdump subprocess'
-    os.kill(tcpdump_pid, signal.SIGKILL)
-    
-    time.sleep(5)
-   
+    wifi_receiver_thread.join()
+    LTE_receiver_thread.join()
 
-    sniff_wifi()
-    sniff_lte()
+
+    #sniff_wifi()
+    #sniff_lte()
 
 
 
--- udp_local.py
+++ udp_local.py
@@ -1,19 +1,15 @@
+__author__ = 'bahar'
 import socket
 import time
 from datetime import datetime
-import sys
+import sys, getopt
 import select
 import pdb
-#from threading import Thread
 import logging
-logging.basicConfig(filename='time_shift.txt',level=logging.DEBUG)
 import netifaces
-import ntplib
 import datetime
-c = ntplib.NTPClient()
 import threading
 import subprocess
-import Queue
 from subprocess import Popen, PIPE
 import objc
 import serial
@@ -28,25 +24,59 @@ from sys import argv
 import psutil
 
 
+#get input options
+#get input options
+try:
+    options, remainder = getopt.getopt(sys.argv[1:], 'a:p:m:s:h', ['SERVER_IP','SERVER_PORT=',
+                                                                 'MODE=',
+                                                                 'PACKET_SIZE=',
+                                                                 'help'                                                             ])
+except getopt.GetoptError as e:
+    print (str(e))
+    print("Usage: %s -a server ip -p server port -m u/d mode -s packet_size in bytes" % sys.argv[0])
+    sys.exit(2)
+for opt, arg in options:
+    if opt in ('-a', '--SERVER_IP'):
+        SERVER_IP = arg
+    if opt in ('-p', '--SERVER_PORT'):
+        SERVER_PORT = int(arg)
+    elif opt in ('-m', '--MODE'):
+        MODE = arg
+    elif opt in ('-s', '--PACKET_SIZE'):
+        PACKET_SIZE =int(arg)
+    elif opt == '--help':
+        print("Usage: %s -a server ip -p server port -m u/d mode -s packet_size in bytes" % sys.argv[0])
+print 'SERVER_IP   :', SERVER_IP
+print 'SERVER_PORT   :', SERVER_PORT
+print 'MODE   :', MODE
+print 'PACKET SIZE    :', PACKET_SIZE
+
+
+
+
+
+
 #DEFINE INPUTS HERE
 #CLIENT - SENDER
-UDP_DEST_IP='134.226.40.138' #IP ADDRESS TO SEND DATAGRAMS TO (v4 or v6)
-UDP_DEST_PORT=60000 #IP PORT TO SEND DATAGRAMS TO
-PACKET_SIZE = 1500 #DATAGRAM SIZE IN BYTES
 NR_OF_PACKETS=2000 #TOTAL NR. OF PACKETS TO SEND
 PACKETS_PER_SEC=100 #PACKETS PER SECON
-rate= float(float(PACKET_SIZE) * float(PACKETS_PER_SEC) *  8 / 1000000 )#data rate mbps
-print ('Transmission rate is:  %3f'  %(rate) + ' Mbit/s')
+RATE= float(float(PACKET_SIZE) * float(PACKETS_PER_SEC) *  8 / 1000000 )#data rate mbps
+print ('Transmission rate is:  %3f'  %(RATE) + ' Mbit/s')
+
 #files and directory assignment
 directory=( '/Users/bahar/Documents/tcpdumpData/python/csv/'+ datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S_%f')[ :-3])
+channels_file='channels.csv'
 if not os.path.exists(directory):
     os.makedirs(directory)
-os.chdir(directory)
-file_name='udp_rtt_' + '%s' %(PACKET_SIZE) +'bytes_' +'%s' %(NR_OF_PACKETS) +'packets'+ '%s' %(PACKETS_PER_SEC)+'sec'+ datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S_%f')[ :-3] +'.csv'
-tcpdump_output='tcpdump_' + '%s' %(PACKET_SIZE) +'bytes_' +'%s' %(NR_OF_PACKETS) +'packets'+ '%s' %(PACKETS_PER_SEC)+'sec' + datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S_%f')[ :-3] + '.csv'
-channels_file='channels.csv'
+    os.chdir(directory)
+if MODE=='uplink' :
+ file_name='udp_rtt_' + '%s' %(PACKET_SIZE) +'bytes_' +'%s' %(NR_OF_PACKETS) +'packets'+ '%s' %(PACKETS_PER_SEC)+'sec'+ datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S_%f')[ :-3] +'.csv'
+ tcpdump_output='tcpdump_' + '%s' %(PACKET_SIZE) +'bytes_' +'%s' %(NR_OF_PACKETS) +'packets'+ '%s' %(PACKETS_PER_SEC)+'sec' + datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S_%f')[ :-3] + '.csv'
+elif MODE=='downlink':
+    file_name='udp_oneway_downlink_' + '%s' %(PACKET_SIZE) +'bytes_' +'%s' %(NR_OF_PACKETS) +'packets'+ '%s' %(PACKETS_PER_SEC)+'sec'+ datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S_%f')[ :-3] +'.csv'
 
-open(channels_file, "a").write('Transmission rate: '+ str(rate)+'  Mbit/s' +'\n')
+#write transmission rate to the channel file
+open(channels_file, "a").write('Transmission rate: '+ str(RATE)+'  Mbit/s' +'\n')
 
 #get ip addresses of the avialable interfaces
 if 'en0' not in netifaces.interfaces():
@@ -74,7 +104,6 @@ wifi_Addr_string=(str(UDP_RECEIVE_IP_wifi) +'.' + str(UDP_RECEIVE_PORT_wifi))
 BUFFER = 4096
 
 #just to get the offset from the ntp server
-
 command = "ntpq -nc peers | tail -n +3 | grep \* | cut -c 62-71| tr -d ' '   "
 ntproc = os.popen(command)
 offset=float(  ntproc.read()) / 1000
@@ -146,7 +175,7 @@ def readAT(serial_port):
      break
 
 
-#function to get wifi parameters
+#function to get wifi parameters --- OS X
 def sniff_wifi():
  # bridge to objective c(apple stuff)
  objc.loadBundle('CoreWLAN',
@@ -160,8 +189,8 @@ def sniff_wifi():
     open(channels_file, "a").write(wifi_parameters+'\n')
 
 
-#CLIENT-RECEIVER PART
-def udp_client_receive(UDP_RECEIVE_IP, UDP_RECEIVE_PORT,offset,input_sock, interface_type):
+#Local-RECEIVE
+def udp_client_receive(IP, PORT,offset,input_sock, interface_type):
  
  global wifi_packet_count_rcvd
  global lte_packet_count_rcvd
@@ -172,7 +201,7 @@ def udp_client_receive(UDP_RECEIVE_IP, UDP_RECEIVE_PORT,offset,input_sock, inter
  wifi_cumulative_delay=0
  lte_cumulative_delay=0
     
-  #FIRE UP THE LISTENER ENGINES
+ #FIRE UP THE LISTENER ENGINES
  while True:
   data, addr = input_sock.recvfrom( BUFFER )
   actual_time=float(time.time()+offset)
@@ -181,37 +210,36 @@ def udp_client_receive(UDP_RECEIVE_IP, UDP_RECEIVE_PORT,offset,input_sock, inter
   timecountnooffset=float(float(timecount)-offset)
   #ntp offset considered
   rt_delay = (actual_time - float(timecount))
-  #ntp offset not considered
-  #rt_delay = (time.time() - float(timecount))
   packet_number = str(splitdata[1].strip("' '"))
   packet_number = packet_number.lstrip('0')
+  client_interface=str(splitdata[2].strip("' '"))
   #WRITE TO FILE AND DO PACKET COUNT
-  outfile = open(file_name, "a").write(str(time.ctime()+','+'received , '+ packet_number+' , '+str(rt_delay)+ ','+  interface_type+'\n'))
+  outfile = open(file_name, "a").write(str(time.ctime()+','+'received , '+ packet_number+' , '+str(rt_delay)+ ','+  client_interface+'\n'))
   #print (time.ctime()+','+'received , '+ packet_number+' , '+ str('%.5f' % timecountnooffset) +'sent time, '+  str('%.5f' % float (actual_time-offset)) + 'received time, ' +str(rt_delay)+'delay')
-  if interface_type=='wifi':
+  if client_interface=='wifi':
    wifi_packet_count_rcvd=wifi_packet_count_rcvd+1
    wifi_cumulative_delay=wifi_cumulative_delay+rt_delay
-  elif interface_type=='lte':
+  elif client_interface=='lte':
    lte_packet_count_rcvd=lte_packet_count_rcvd+1
    lte_cumulative_delay=lte_cumulative_delay+rt_delay
 
 
   
-#CLIENT SERVER SIDE
-def udp_client_send(UDP_DEST_IP, UDP_DEST_PORT, PACKET_SIZE, NR_OF_PACKETS, PACKETS_PER_SEC,offset, input_sock,interface_type):
+#Local-Send
+def udp_client_send(IP, PORT, PACKET_SIZE, NR_OF_PACKETS, PACKETS_PER_SEC,offset, input_sock,interface_type):
  #sleep  1 secs so tcpdump starts earlier
  time.sleep(1)
  inter_departure_time = 1./PACKETS_PER_SEC
  wifi_packet_count_snd=0
  lte_packet_count_snd=0
  print 'UDP Client Started' , '\n'
- print 'UDP target IP:', UDP_DEST_IP, '\n'
- print 'UDP target port:', UDP_DEST_PORT, '\n'
+ print 'UDP target IP:', SERVER_IP, '\n'
+ print 'UDP target port:', SERVER_PORT, '\n'
  print 'UDP Packets to Send:', NR_OF_PACKETS, '\n'
 
  
  #IF IPv6
- if ':' in UDP_DEST_IP:
+ if ':' in SERVER_IP:
   if PACKET_SIZE > 97:
    #BUILD DATAGRAM OF DESIRED SIZE
    padding=''
@@ -221,7 +249,7 @@ def udp_client_send(UDP_DEST_IP, UDP_DEST_PORT, PACKET_SIZE, NR_OF_PACKETS, PACK
      #SEND SPECIFIED NUMBER OF PACKETS
      time.sleep(inter_departure_time )
      snd_sock6 = socket.socket( socket.AF_INET6,socket.SOCK_DGRAM )
-     snd_sock6.sendto(str(("%.5f" % time.time(),str('%08d' % i), padding)), (UDP_DEST_IP, UDP_DEST_PORT) )
+     snd_sock6.sendto(str(("%.5f" % time.time(),str('%08d' % i), padding)), (IP, PORT) )
      packet_count_snd = packet_count_snd+1
  
  #IF NOT IPv6
@@ -232,21 +260,7 @@ def udp_client_send(UDP_DEST_IP, UDP_DEST_PORT, PACKET_SIZE, NR_OF_PACKETS, PACK
      padding = padding+str(1)
    for i in range (1,NR_OF_PACKETS+1):
      time.sleep(inter_departure_time)
-     #snd_sock = socket.socket( socket.AF_INET,socket.SOCK_DGRAM )
-     #Try to bind to send socket??
-     #try:
-      #snd_sock.bind(('0.0.0.0',65000))
-      #print ("Clie", ADDR )
-     #except Exception:
-      #print '***ERROR: Client Port Binding Failed'
-     #timestamp+ntp offset
-     input_sock.sendto(str(("%.5f" % float(time.time()+ offset),str('%08d' % i), interface_type, padding)), (UDP_DEST_IP, UDP_DEST_PORT) )
-     #print (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]+','+'sent, '+ str('%02d' % i)+ interface_type+'\n')
-     #print (datetime.datetime.now()  +  ',' + 'sent, ' + str('%08d' %i) )
-     #ntp offset is not considered
-     #input_sock.sendto(str(("%.5f" % float(time.time()),str('%08d' % i), padding)), (UDP_DEST_IP, UDP_DEST_PORT) )
-     #was there to log time shift- but may be this adds additional delay so we only do it once
-     #logging.info(txt)
+     input_sock.sendto(str(("%.5f" % float(time.time()+ offset),str('%08d' % i), interface_type, padding)), (IP, PORT) )
      if interface_type=='wifi':
       wifi_packet_count_snd = wifi_packet_count_snd+1
      elif interface_type=='lte':
@@ -254,24 +268,6 @@ def udp_client_send(UDP_DEST_IP, UDP_DEST_PORT, PACKET_SIZE, NR_OF_PACKETS, PACK
  
  #WAIT 5SEC FOR ALL PACKETS TO ARRIVE
  time.sleep(5)
- if interface_type=='wifi':
-  wifi_PLR = 100 - ((wifi_packet_count_rcvd*100.)/wifi_packet_count_snd)
-  print '\n', wifi_packet_count_snd, 'wifi packets sent','\n'
-  print wifi_packet_count_rcvd, 'wifi packets received','\n'
-  print 'wifi packet loss ratio = ', round(wifi_PLR, 3), '%','\n'
-  if wifi_packet_count_rcvd == 0:
-   pass
-  else:
-   print 'average wifi rtt = ', wifi_cumulative_delay/wifi_packet_count_rcvd,'\n'
- elif interface_type=='lte':
-  lte_PLR = 100 - ((lte_packet_count_rcvd*100.)/lte_packet_count_snd)
-  print '\n', lte_packet_count_snd, 'lte packets sent','\n'
-  print lte_packet_count_rcvd, 'lte packets received','\n'
-  print 'lte packet loss ratio = ', round(lte_PLR, 3), '%','\n'
-  if lte_packet_count_rcvd == 0:
-   pass
-  else:
-   print 'average lte rtt = ', lte_cumulative_delay/lte_packet_count_rcvd,'\n'
 
 
 #function to call tcpdump
@@ -283,15 +279,11 @@ def Readtraffic():
  tcpdump_wifi_count=0
  command = 'sudo tcpdump host 134.226.40.138 udp'
  dumpproc = subprocess.Popen(['sudo', 'tcpdump', '-n', 'dst' ,'host', '134.226.40.138'] , bufsize=0, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
- #dumpproc = subprocess.Popen(command,executable="/bin/bash", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
  global tcpdump_pid
  tcpdump_pid=dumpproc.pid
  print('tcpdump pid is:'+str(tcpdump_pid))
  #output = ''
  for line in iter(dumpproc.stdout.readline, ""):
- #while True:
-  #line=dumpproc.stdout.readline()
-  #if subprocess.Popen.poll(dumpproc) is not None:
   if len(line.split()) > 2 :
    line_split=line.split()
    if line_split[1]=='IP':
@@ -315,27 +307,25 @@ def Readtraffic():
    break
  print('DONE')
 
-#def kill_processes():
 
 
 
 #START THE THREADS FOR SENDER AND RECEIVER
 if __name__ == "__main__":
     
+ if MODE=='uplink':
     sniff_wifi()
     sniff_lte()
     tcpdump_thread=threading.Thread(target=Readtraffic, args=())
     #tcpdump_thread.daemon=True
     tcpdump_thread.start()
     #time.sleep(1)
-
-    
-    
-    wifi_sender_thread = threading.Thread(target=udp_client_send, args=(UDP_DEST_IP, UDP_DEST_PORT, PACKET_SIZE, NR_OF_PACKETS, PACKETS_PER_SEC,offset,wifi_sock,'wifi'))
+    #initiate  send/recv threaths
+    wifi_sender_thread = threading.Thread(target=udp_client_send, args=(SERVER_IP, SERVER_PORT, PACKET_SIZE, NR_OF_PACKETS, PACKETS_PER_SEC,offset,wifi_sock,'wifi'))
     #wifi_sender_thread.daemon=True
     wifi_sender_thread.start()
     
-    LTE_sender_thread = threading.Thread(target=udp_client_send, args=(UDP_DEST_IP, UDP_DEST_PORT, PACKET_SIZE, NR_OF_PACKETS, PACKETS_PER_SEC,offset,LTE_sock,'lte'))
+    LTE_sender_thread = threading.Thread(target=udp_client_send, args=(SERVER_IP, SERVER_PORT, PACKET_SIZE, NR_OF_PACKETS, PACKETS_PER_SEC,offset,LTE_sock,'lte'))
     #LTE_sender_thread.daemon=True
     LTE_sender_thread.start()
     
@@ -356,12 +346,30 @@ if __name__ == "__main__":
     time.sleep(5)
     print 'killing tcpdump subprocess'
     os.kill(tcpdump_pid, signal.SIGKILL)
-    
     time.sleep(5)
-   
-
     sniff_wifi()
     sniff_lte()
+ 
+ elif MODE=='downlink':
+  #send one packet by wifi interface to handshake with the server
+  udp_client_send(SERVER_IP, SERVER_PORT, PACKET_SIZE, 1, PACKETS_PER_SEC,offset,wifi_sock,'wifi')
+  #send one packet by LTE interface to handshake with the NAT
+  udp_client_send(SERVER_IP, SERVER_PORT, PACKET_SIZE, 1, PACKETS_PER_SEC,offset,LTE_sock,'lte')
+  #start receiver threaths
+  wifi_receiver_thread = threading.Thread(target=udp_client_receive, args=(UDP_RECEIVE_IP_wifi, UDP_RECEIVE_PORT_wifi,offset,wifi_sock,'wifi'))
+  wifi_receiver_thread.daemon=True
+  wifi_receiver_thread.start()
+  LTE_receiver_thread = threading.Thread(target=udp_client_receive, args=(UDP_RECEIVE_IP_LTE, UDP_RECEIVE_PORT_LTE,offset,LTE_sock,'lte'))
+  LTE_receiver_thread.daemon=True
+  LTE_receiver_thread.start()
+  #time.sleep(1)
+  #close all threads
+  sniff_wifi()
+  sniff_lte()
+
+
+
+
 
 
 
