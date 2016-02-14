1bd12578e40ff9a2d50018e0dec093f765ef1309
baharxy <bpartovp@gmail.com>
2016-02-14 12:29:25 +0000

    improved compatibility

--- atcommands.py
+++ atcommands.py
@@ -3,25 +3,43 @@ import pdb
 from curses import ascii
 from serial.tools import list_ports
 import time
+import os 
+from sys import platform as _platform 
+
+if _platform == "linux" or _platform == "linux2":
+  plat = 'LINUX'
+  print "The operating system is Linux"
+elif _platform == "darwin":
+  plat = 'OS_X'
+  import objc
+  print  "The operating system is os x"
 
 #pdb.set_trace()
-ser=serial.Serial('/dev/cu.JRDDeviceInterface000010141', 115200, timeout=3)
+if plat=='OS_X':
+ ser=serial.Serial('/dev/cu.JRDDeviceInterface000010141', 115200, timeout=3)
+elif plat=='LINUX':
+ ser=serial.Serial('/dev/ttyUSB2', 115200, timeout=3)
+
 #check if any thing is in the buffer
 if ser.inWaiting() > 0:
     ser.flushInput()
 #send command
-command= ser.write('AT*CNTI=0\r')
+command= ser.write("AT*CNTI=0\r")
+if plat=='LINUX':
+ response=ser.read(1024)
+ print response
+ ser.close()
 #time.sleep(5)
 #command=ser.write('AT+CREG?\r')
 #command = ser.write('AT!GSTATUS?\r')
 #print command
 #pdb.set_trace()
 #read resposnse
-
-while 1:
+elif plat=='OS_X':
+ while 1:
    if ser.inWaiting():
       msg = ser.read(ser.inWaiting())
       print msg
-print msg
-ser.close()
+ print msg
+ ser.close()
 
--- sniff_wifi.py
+++ sniff_wifi.py
@@ -1,15 +1,30 @@
-import objc
-objc.loadBundle('CoreWLAN',
+import os
+import sys
+from sys import platform as _platform
+import time
+import types
+import pythonwifi.flags
+from pythonwifi.iwlibs import Wireless, WirelessInfo, Iwrange, getNICnames, getWNICnames
+     
+if _platform== 'linux' or _platform== 'linux2':
+ interface=Wireless('wlan1')
+ stat, qual, discard, missed_beacon = interface.getStatistics()
+ # Link Quality, Signal Level and Noise Level line
+ wifi_parameters= 'Interface:      %s, SSID:           %s, Transmit Rate:  %s, Transmit Power: %s, RSSI:           %s'   % ('wlan1', interface.getEssid(), interface.getBitrate(), interface.getTXPower(), qual.signallevel)
+ print wifi_parameters
+elif _platform=='darwin':
+ import objc
+ objc.loadBundle('CoreWLAN',
                 bundle_path='/System/Library/Frameworks/CoreWLAN.framework',
                 module_globals=globals())
 
-for iname in CWInterface.interfaceNames():
+ for iname in CWInterface.interfaceNames():
   interface = CWInterface.interfaceWithName_(iname)
   print """
-Interface:      %s
-SSID:           %s
-Transmit Rate:  %s
-Transmit Power: %s
-RSSI:           %s""" % (iname, interface.ssid(), interface.transmitRate(),
+ Interface:      %s
+ SSID:           %s
+ Transmit Rate:  %s
+ Transmit Power: %s
+ RSSI:           %s""" % (iname, interface.ssid(), interface.transmitRate(),
                          interface.transmitPower(), interface.rssi())
 
